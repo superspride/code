a45ffc140da2eec3a1d30a75d0b28c6f6f4c6390
li <2528110839@qq.com>
2017-06-12 11:34:52 +0800

    第一次

---
+++ kfsy_lishibo/src/main/java/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+
+import com.baizhi.App;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class Test {
+    public static void main(String[] args) {
+        App.md5andsalt();
+    }
+}
---
+++ lishibo_commons/src/main/java/com/baizhi/App.java
@@ -0,0 +1,11 @@
+package com.baizhi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+   public static void md5andsalt(){
+
+   }
+}
